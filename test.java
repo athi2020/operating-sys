bcca052cbee0849f68cbda50e9c136fb3ad00cc5
athi2020 <athiralekshmi2020@gmail.com>
2025-04-30 14:34:16 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,18 @@
+import Graphics.Op;
+public class test{
+    public static void main(String[]args)
+    {
+        Op A=new Op();
+        System.out.println ("****OPERATIONS****");
+        System.out.println("\n___ADDITION___");
+        A.add();
+        System.out.println("\n___SUBSTRATION___");
+        A.sub();
+        System.out.println("\n___MULITIPLICATION___");
+        A.mul();
+        System.out.println("\n___DIVISION___");
+        A.div();
+        
+
+    }
+}
