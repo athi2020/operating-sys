2ffba6befe717937a5859e4cd9a201a69e8ef913
athi2020 <athiralekshmi2020@gmail.com>
2025-05-07 10:54:06 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,41 +1,32 @@
 import java.util.Scanner;
-class Employee {
-    public String eNo;
-    public String eName;
-    public int eSalary;
-
-    public Employee(String eNo, String eName, int eSalary) {
-        this.eNo = eNo;
-        this.eName = eName;
-        this.eSalary = eSalary;
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter count of employee list : ");
-        int count = Integer.parseInt(scan.nextLine());
-        Employee[] employees = new Employee[count];
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.print("Enter the value of n for Fibonacci series: ");
+        int n = scanner.nextInt();
 
-        for (int i = 0; i < count; i++) {
-            System.out.print("Enter Number of employee : ");
-            String eNo = scan.nextLine();
-            System.out.print("Enter Name of employee : ");
-            String eName = scan.nextLine();
-            System.out.print("Enter Salary of employee : ");
-            int eSalary = Integer.parseInt(scan.nextLine());
-            employees[i] = new Employee(eNo, eName, eSalary);
-        }
+        
+        System.out.print("Enter the range (start end): ");
+        int start = scanner.nextInt();
+        int end = scanner.nextInt();
 
-        System.out.println("Enter the number of employee to be filtered out :");
-        String search = scan.nextLine();
-        for (int i = 0; i < count; i++) {
-            if (employees[i].eNo.equals(search)) {
-                System.out.println("Employee Number : " + employees[i].eNo);
-                System.out.println("Employee Name : " + employees[i].eName);
-                System.out.println("Employee Salary : " + employees[i].eSalary);
-            }
-        }
+       
+       
+        EvenNumberPrinter evenNumberPrinter = new EvenNumberPrinter(start, end);
+ 
+	Thread evenThread = new Thread(evenNumberPrinter);
+ 
+	evenThread.start();
+ 
+ FibonacciGenerator fibonacciGenerator = new FibonacciGenerator(n);
+       
+        Thread fibonacciThread = new Thread(fibonacciGenerator);
+        
+
+       
+        fibonacciThread.start();
+        
     }
 }
